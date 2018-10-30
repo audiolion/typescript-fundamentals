2099d628ed8ffc5d09aac757c2888a240360ebc6
Ryan Castner <castner.rr@gmail.com>
2018-10-30 17:04:27 -0400

    Remove old decorator stuff

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,8 @@
 import { generateRandomId, Component } from "./utils";
 
-function prop(x, name) {
-  console.log(x, name);
-}
-
-function param(x, name, index) {
-  console.log(x, name, index);
-}
-
 @Component({ id: "app" })
 class App {
-  @prop
-  static version?: string;
-
-  @prop
-  version2?: string;
-
-  onInit(@param ele: HTMLElement | null): void {
+  onInit(ele: HTMLElement | null): void {
     setInterval(function() {
       if (ele) {
         ele.innerHTML = generateRandomId("$", 7);
