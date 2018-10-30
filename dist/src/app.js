5c22c5cef0dd8de3bd47a3917c7a3c71afdb1fbf
Ryan Castner <castner.rr@gmail.com>
2018-10-30 15:36:11 -0400

    Add class properties and App class

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,6 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var utils_1 = require("./utils");
+var App = /** @class */ (function () {
+    function App() {
+        this.id = "app";
+    }
+    return App;
+}());
 function main() {
     var app = document.getElementById("app");
     setInterval(function () {
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import { generateRandomId } from "./utils";
 
+class App {
+  id: string;
+  constructor() {
+    this.id = "app";
+  }
+}
+
 function main() {
   let app = document.getElementById("app");
   setInterval(function() {
